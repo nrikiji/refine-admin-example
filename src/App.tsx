d8a9ef8197a648950f5f1fac4eb4b04ed3439beb
nrikiji <nrikiji@gmail.com>
2022-05-20 10:20:24 +0900

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  Layout,
+  ReadyPage,
+  ErrorComponent,
+} from "@pankod/refine-antd";
+import "@pankod/refine-antd/dist/styles.min.css";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+import { useTranslation } from "react-i18next";
+import { Header } from "components/layout";
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <Refine
+      notificationProvider={notificationProvider}
+      Layout={Layout}
+      ReadyPage={ReadyPage}
+      catchAll={<ErrorComponent />}
+      routerProvider={routerProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      i18nProvider={i18nProvider}
+      Header={Header}
+    />
+  );
+}
+
+export default App;
